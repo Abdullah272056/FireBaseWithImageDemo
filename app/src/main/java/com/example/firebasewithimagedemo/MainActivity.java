04e9f4382821b465d5bfbb1b7144ddfb6c125c34
Abdullah272056 <abdullah272056@gmail.com>
2020-11-02 03:55:28 +0600

    finding view and button's set onClickListener

--- app/src/main/java/com/example/firebasewithimagedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasewithimagedemo/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.firebasewithimagedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button chooseButton,saveButton,displayButton;
+    ProgressBar progressBar;
+    EditText imageNameExitText;
+    ImageView imageView;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //finding view
+        chooseButton=findViewById(R.id.chooseImageButtonId);
+        saveButton=findViewById(R.id.saveImageButtonId);
+        displayButton=findViewById(R.id.displayImageButtonId);
+        progressBar=findViewById(R.id.progressbarId);
+        imageNameExitText=findViewById(R.id.imageNameEditTextIdId);
+        imageView=findViewById(R.id.imageViewId);
+
+        //add listener
+        chooseButton.setOnClickListener(this);
+        saveButton.setOnClickListener(this);
+        displayButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+    switch (v.getId()){
+        case R.id.chooseImageButtonId:
+
+            break;
+        case R.id.saveImageButtonId:
+
+            break;
+        case  R.id.displayImageButtonId:
+
+            break;
+    }
     }
 }
