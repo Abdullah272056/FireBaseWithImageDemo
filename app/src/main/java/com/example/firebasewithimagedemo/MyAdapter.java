fce80736243954615fd5ed1c3ea98634f688c56d
Abdullah272056 <abdullah272056@gmail.com>
2020-11-03 11:22:13 +0600

    MyAdapter class extend recyclerview and implements override method

--- app/src/main/java/com/example/firebasewithimagedemo/MyAdapter.java
+++ app/src/main/java/com/example/firebasewithimagedemo/MyAdapter.java
@@ -1,5 +1,32 @@
 package com.example.firebasewithimagedemo;
 
-public class MyAdapter {
-    
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
