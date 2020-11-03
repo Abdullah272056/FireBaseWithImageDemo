fc94b9ba1021d6d8a83e2585b0e3b9be86dbeaf7
Abdullah272056 <abdullah272056@gmail.com>
2020-11-03 11:24:21 +0600

    create constructor in My Adapter class

--- app/src/main/java/com/example/firebasewithimagedemo/MyAdapter.java
+++ app/src/main/java/com/example/firebasewithimagedemo/MyAdapter.java
@@ -1,12 +1,22 @@
 package com.example.firebasewithimagedemo;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context context;
+    private List<UploadModelClass> uploadList;
+
+    public MyAdapter(Context context, List<UploadModelClass> uploadList) {
+        this.context = context;
+        this.uploadList = uploadList;
+    }
 
     @NonNull
     @Override
