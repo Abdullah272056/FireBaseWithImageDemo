3cffb1c76d2c496d8893a7597fe554429cf9e100
Abdullah272056 <abdullah272056@gmail.com>
2020-11-03 12:43:32 +0600

    variable declaration and recyclerView Finding

--- app/src/main/java/com/example/firebasewithimagedemo/ImageActivity.java
+++ app/src/main/java/com/example/firebasewithimagedemo/ImageActivity.java
@@ -1,17 +1,29 @@
 package com.example.firebasewithimagedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.google.firebase.database.DatabaseReference;
 
+import java.util.List;
+
 public class ImageActivity extends AppCompatActivity {
 
-   
+    DatabaseReference databaseReference;
+    MyAdapter myAdapter;
+    List<Upload>uploadList;
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+        recyclerView=findViewById(R.id.recyclerviewId);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
