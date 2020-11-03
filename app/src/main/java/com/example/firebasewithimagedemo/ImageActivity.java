92faaf06ce0115f65a87654fabe2649217cb4001
Abdullah272056 <abdullah272056@gmail.com>
2020-11-03 12:48:24 +0600

    database reference initialize and addValueEventListener

--- app/src/main/java/com/example/firebasewithimagedemo/ImageActivity.java
+++ app/src/main/java/com/example/firebasewithimagedemo/ImageActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebasewithimagedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,8 +8,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImageActivity extends AppCompatActivity {
@@ -25,5 +31,18 @@ public class ImageActivity extends AppCompatActivity {
         recyclerView=findViewById(R.id.recyclerviewId);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        uploadList=new ArrayList<>();
+        databaseReference= FirebaseDatabase.getInstance().getReference("Upload");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
